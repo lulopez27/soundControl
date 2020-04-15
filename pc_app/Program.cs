d7faa956ed2aed25d34f6be1d70fc95ee28c6312
lulopez <lulopez@estudiantec.cr>
2020-04-15 15:44:41 -0600

    First commit, has sound implementations and basic for a server, will start working on app to test server

---
+++ pc_app/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+namespace pc_app
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            AudioUtilities.SetMasterVolume(75f);
+            System.Console.WriteLine($"This is system audio {AudioUtilities.GetMasterVolume()}" );
+            System.Threading.Thread.Sleep(5000);
+            AudioUtilities.SetMasterVolume(50f);
+            System.Console.WriteLine($"This is system audio {AudioUtilities.GetMasterVolume()}" );
+            System.Threading.Thread.Sleep(5000);
+            Console.WriteLine("Friendly names:");
+            List<AudioDevice> soundDevices = new List<AudioDevice>();
+            foreach (AudioDevice device in AudioUtilities.GetAllDevices())
+            {
+                Console.WriteLine(device.FriendlyName);
+                soundDevices.Add(device);
+            }
+
+            // dump all audio sessions
+            List<Process> audioSess = new List<Process>();
+            Console.WriteLine("Process names:");
+            foreach (AudioSession session in AudioUtilities.GetAllSessions())
+            {
+                if (session.Process != null)
+                {
+                    // only the one associated with a defined process
+                    audioSess.Add(session.Process);
+                }
+            }
+            AudioUtilities.SetApplicationVolume(audioSess[0].Id,50);
+            System.Threading.Thread.Sleep(5000);
+            AudioUtilities.SetApplicationVolume(audioSess[0].Id,100);
+        }
+    }
+}
---
+++ pc_app/VolumeControl.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace pc_app
+{
+    public class VolumeControl
+    {
+        private const byte VK_VOLUME_MUTE = 0xAD;
+        private const byte VK_VOLUME_DOWN = 0xAE;
+        private const byte VK_VOLUME_UP = 0xAF;
+        private const UInt32 KEYEVENTF_EXTENDEDKEY = 0x0001;
+        private const UInt32 KEYEVENTF_KEYUP = 0x0002;
+
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, UInt32 dwFlags, UInt32 dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        static extern Byte MapVirtualKey(UInt32 uCode, UInt32 uMapType);
+
+        public static void VolumeUp()
+        {
+            keybd_event(VK_VOLUME_UP, MapVirtualKey(VK_VOLUME_UP, 0), KEYEVENTF_EXTENDEDKEY, 0);
+            keybd_event(VK_VOLUME_UP, MapVirtualKey(VK_VOLUME_UP, 0), KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+
+        public static void VolumeDown()
+        {
+            keybd_event(VK_VOLUME_DOWN, MapVirtualKey(VK_VOLUME_DOWN, 0), KEYEVENTF_EXTENDEDKEY, 0);
+            keybd_event(VK_VOLUME_DOWN, MapVirtualKey(VK_VOLUME_DOWN, 0), KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+
+        public static void Mute()
+        {
+            keybd_event(VK_VOLUME_MUTE, MapVirtualKey(VK_VOLUME_MUTE, 0), KEYEVENTF_EXTENDEDKEY, 0);
+            keybd_event(VK_VOLUME_MUTE, MapVirtualKey(VK_VOLUME_MUTE, 0), KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+
+        public static float GetMasterVolume()
+        {
+            // get the speakers (1st render + multimedia) device
+            IMMDeviceEnumerator deviceEnumerator = (IMMDeviceEnumerator)(new MMDeviceEnumerator());
+            IMMDevice speakers;
+            const int eRender = 0;
+            const int eMultimedia = 1;
+            deviceEnumerator.GetDefaultAudioEndpoint(eRender, eMultimedia, out speakers);
+
+            object o;
+            speakers.Activate(typeof(IAudioEndpointVolume).GUID, 0, IntPtr.Zero, out o);
+            IAudioEndpointVolume aepv = (IAudioEndpointVolume)o;
+            float volume = aepv.GetMasterVolumeLevelScalar();
+            Marshal.ReleaseComObject(aepv);
+            Marshal.ReleaseComObject(speakers);
+            Marshal.ReleaseComObject(deviceEnumerator);
+            return volume;
+        }
+
+        [ComImport]
+        [Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")]
+        private class MMDeviceEnumerator
+        {
+        }
+
+        [Guid("5CDF2C82-841E-4546-9722-0CF74078229A"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IAudioEndpointVolume
+        {
+            void _VtblGap1_6();
+            float GetMasterVolumeLevelScalar();
+        }
+
+        [Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IMMDeviceEnumerator
+        {
+            void _VtblGap1_1();
+
+            [PreserveSig]
+            int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice ppDevice);
+        }
+
+        [Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IMMDevice
+        {
+            [PreserveSig]
+            int Activate([MarshalAs(UnmanagedType.LPStruct)] Guid iid, int dwClsCtx, IntPtr pActivationParams, [MarshalAs(UnmanagedType.IUnknown)] out object ppInterface);
+        }
+    }
+}
